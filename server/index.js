b114145cfc58f670c06e4831c64c00451d265f43
Haley Boyle <haleyjboyle@gmail.com>
2017-10-30 20:50:23 -0700

    server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-asdf
+const express = require('express'),
+bodyParser = require('body-parser'),
+massive = require('massive'),
+session = require('express-session'),
+cors =  require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = 6000;
+
+app.listen(PORT, ()=> console.log('Listening on port:' , PORT))
