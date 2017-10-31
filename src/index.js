1329f4889d6b539537c3c6c3e1a885e5c3d16c74
Annie  Nguyen <annienguyen@Annies-MacBook-Air.local>
2017-10-31 11:59:15 -0700

    message here

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/main.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { HashRouter } from "react-router-dom";
+
+ReactDOM.render(
+<HashRouter>
+    <App />
+    </HashRouter>,
+    document.getElementById('root'));
 registerServiceWorker();
