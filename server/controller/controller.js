ff185f3eb591cbb9a915070f112c82e025b7927f
Will Twitty <willtwitty@Wills-MacBook-Pro-2.local>
2017-10-31 11:54:04 -0700

    initial server, testing get menu type

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,3 +1,11 @@
 module.exports = {
+    getMenuType(req, res) {
+        const db = req.app.get('db')
+    db.get_menu_type([req.params.type]).then(response => {
+        console.log(response);
+
+    }).catch((err) => {console.log(err)})
+
     
+    }
 }
