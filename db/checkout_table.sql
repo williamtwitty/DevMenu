d86347eb14a4991a3a535d2936249dcd3ec8b3b4
Will Twitty <willtwitty@Wills-MacBook-Pro-2.local>
2017-10-31 11:19:11 -0700

    server stuff

---
+++ db/checkout_table.sql
@@ -0,0 +1,4 @@
+create table checkout(
+order_id references order(id),
+table_number int,
+total decimal )
---
+++ db/testing.sql
@@ -0,0 +1,7 @@
+-- insert into menu(price, name, type, chicken, bread, eggs)
+-- values
+-- (4.99, Nuggs, entree, 12, 2, 3);
+
+-- insert into menu(price, name, type, lettuce, croutons, tomatoes, cucumbers)
+-- values
+-- (7.99, 'garden salad', 'salad', 3, 13, 1, 5);
