f3ea893bfe66eb65342e61a073d70ff688c6271a
Will Twitty <willtwitty@Wills-MacBook-Pro-2.local>
2017-11-03 16:12:59 -0700

    fixing merges

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -52,7 +52,8 @@ class MenuItems extends Component {
                         </div>
                     </div>
                 </div>
-        ); 
+          </div>
+        )
     }
 }
 function mapStateToProps(state){
