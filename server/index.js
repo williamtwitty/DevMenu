4a78015b713ab9a02c68797edb46e96559d5d69d
Annie  Nguyen <annienguyen@Annies-MacBook-Air.local>
2017-11-16 10:09:26 -0700

    trying to host

--- server/index.js
+++ server/index.js
@@ -63,8 +63,7 @@ passport.deserializeUser(function(userId, done) {
 app.get('/auth', passport.authenticate('auth0'))
 
 app.get('/auth/callback', passport.authenticate('auth0', {
-    successRedirect: `${process.env.SERVERHOST}/#/admin`,  
-    //comment
+    successRedirect: `${process.env.SERVERHOST}/#/admin`,
     failureRedirect: '/auth'
 }))
 
