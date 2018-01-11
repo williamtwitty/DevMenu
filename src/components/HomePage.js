f03a3474c48ca82f6375838e91855b29bf506d74
Haley <haleyjboyle@gmail.com>
2018-01-11 11:12:50 -0700

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,25 +9,16 @@ class HomePage extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            // tableNum: ''
-        }
+       }
         this.handleOnChange = this.handleOnChange.bind(this);
-        // this.handleOnClick = this.handleOnClick.bind(this);
     }
 
     handleOnChange(e) {
-        // this.setState = {
-        //     tableNum: e
-        //     }
         return this.props.selectTableNumber(e); 
     }
 
     handleOnClick(e) {
-        // this.setState = {
-        //     tableNum: e
-        //     }
-        // return this.props.selectTableNumber(e);
-    }
+     }
         
     
 
@@ -69,15 +60,7 @@ class HomePage extends Component {
                             this.props.history.push("/");
                         }
                    
-                        // (e.target.value === undefined) 
-                        // ?
-                        // this.handleOnClick(e.target.value)}}> 
-                        // <Link to = '/'>  orders</Link> </button>
-                        // :
-                        // this.handleOnClick(e.target.value)}}> 
-                        // <Link to = '/menu'>  orders</Link> </button>
-                        
-                         this.handleOnClick(e.target.value)}}> 
+                        this.handleOnClick(e.target.value)}}> 
                             <img alt="" src='https://imgur.com/KdSyY3c.jpg'width='25px'height='25px'/>
                             <img alt="" className='hover-arrow' src='https://imgur.com/KdSyY3c.jpg'width='20px'height='20px'/>
                             <img alt="" className='hover-arrow2' src='https://imgur.com/KdSyY3c.jpg'width='16.5px'height='16.5px'/>
